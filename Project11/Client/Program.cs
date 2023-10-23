222ecec53fbdd7c6b2a1bc7c578a4ae222fd27b5
VladaPR26 <116334021+VladaPR26@users.noreply.github.com>
2023-10-23 17:26:41 +0200

    Add files via upload
    Add components and conections.

---
+++ Project11/Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<IMonitoringServerTest> channelMontoring =
+               new ChannelFactory<IMonitoringServerTest>("MonitoringServer");
+            IMonitoringServerTest proxyMonitoring = channelMontoring.CreateChannel();
+            string testMonitoringServer;
+            Console.WriteLine("To monitoring server: ");
+            testMonitoringServer=Console.ReadLine();
+            proxyMonitoring.TestMonitoringServer(testMonitoringServer);
+
+            ChannelFactory<IServerTest> channelServer =
+               new ChannelFactory<IServerTest>("Server");
+            IServerTest proxyServer = channelServer.CreateChannel();
+            string testServer;
+            Console.WriteLine("To server: ");
+            testServer = Console.ReadLine();
+            proxyServer.TestServer(testServer);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Project11/MonitoringServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(MonitoringServerTest)))
+            {
+                host.Open();
+                Console.WriteLine("Monitoring server started successfully");
+                Console.ReadKey();
+            }
+        }
+    }
+}
